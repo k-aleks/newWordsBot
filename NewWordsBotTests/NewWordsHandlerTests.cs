f6ae7c0bc00cba0da4d87f87195b452584a11c2c
Aleksandr Kazakov <kazakoa@amazon.com>
2017-09-30 20:27:17 +0100

    Completed with tests for NewWordsHandler

--- NewWordsBotTests/NewWordsHandlerTests.cs
+++ NewWordsBotTests/NewWordsHandlerTests.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using NewWordsBot;
 using NSubstitute;
-using Telegram.Bot;
 using Telegram.Bot.Types;
 using Xunit;
 
@@ -58,49 +58,138 @@ namespace NewWordsBotTests
         [Fact]
         public void TryHandleCallback_should_return_false_if_no_pending_definitions()
         {
-           Assert.True(false, "TBD"); 
+            var handler = new NewWordsHandler(MockUsersStorage(), MockWordsStorage(), MockWordsDictionary(), MockNewWordsHandlerMessanger(), MockLearningMethodology());
+
+            CallbackQuery callback = new CallbackQuery()
+            {
+               Data = "/notadd"
+            };
+            
+            handler.TryHandleCallback(callback).Should().BeFalse();
         }
         
         [Fact]
         public void TryHandleCallback_should_call_methodology_for_creating_new_word_and_save_word_to_storage_and_return_true()
         {
-           Assert.True(false, "TBD"); 
+            var wordToLearn = "word to learn";
+            var definitions = new List<string>()
+            {
+                "definition one",
+                "definition two",
+                "definition three",
+            };
+            var user = new NewWordsBot.User("myUserName", 12345);
+            var word = new Word(wordToLearn, definitions[1], WordForm.Noun, LearningStage.First_1m, DateTime.MinValue, DateTime.Now);
+
+            var usersStorage = MockUsersStorage();
+            usersStorage.GetOrRegisterUser(null).ReturnsForAnyArgs(user);
+
+            var dictionary = MockWordsDictionary();
+            dictionary.Find(wordToLearn).Returns(new DictionaryItem(wordToLearn, definitions, WordForm.Noun));
+
+            var messanger = MockNewWordsHandlerMessanger();
+
+            var learningMethodology = MockLearningMethodology();
+            learningMethodology.CreateNewWord(wordToLearn, definitions[1], WordForm.Noun).Returns(word);
+            var wordsStorage = MockWordsStorage();
+            var newWordsHandler = new NewWordsHandler(usersStorage, wordsStorage, dictionary, messanger, learningMethodology);
+            var handler = newWordsHandler;
+
+            Message message = new Message
+            {
+                Text = "/add " + wordToLearn,
+            };
+
+            handler.TryHandleRequest(message).Should().BeTrue();
+
+            CallbackQuery callback = new CallbackQuery()
+            {
+               Data = "/add 1",
+               Message = new Message()
+            };
+            
+            handler.TryHandleCallback(callback).Should().BeTrue();
+            learningMethodology.Received().CreateNewWord(wordToLearn, definitions[1], WordForm.Noun);
+            wordsStorage.Received().AddOrUpdate(user, word);
+            messanger.Received().SendNewWordConfirmation(user, word);
         }
         
         [Fact]
         public void TryHandleCallback_should_not_do_nothing_on_second_callback_and_return_false()
         {
-           Assert.True(false, "TBD"); 
+            var wordToLearn = "word to learn";
+            var definitions = new List<string>()
+            {
+                "definition one",
+                "definition two",
+                "definition three",
+            };
+            var user = new NewWordsBot.User("myUserName", 12345);
+            var word = new Word(wordToLearn, definitions[1], WordForm.Noun, LearningStage.First_1m, DateTime.MinValue, DateTime.Now);
+
+            var usersStorage = MockUsersStorage();
+            usersStorage.GetOrRegisterUser(null).ReturnsForAnyArgs(user);
+
+            var dictionary = MockWordsDictionary();
+            dictionary.Find(wordToLearn).Returns(new DictionaryItem(wordToLearn, definitions, WordForm.Noun));
+
+            var messanger = MockNewWordsHandlerMessanger();
+
+            var learningMethodology = MockLearningMethodology();
+            learningMethodology.CreateNewWord(wordToLearn, definitions[1], WordForm.Noun).Returns(word);
+            var wordsStorage = MockWordsStorage();
+            var newWordsHandler = new NewWordsHandler(usersStorage, wordsStorage, dictionary, messanger, learningMethodology);
+            var handler = newWordsHandler;
+
+            Message message = new Message
+            {
+                Text = "/add " + wordToLearn,
+            };
+
+            handler.TryHandleRequest(message).Should().BeTrue();
+
+            CallbackQuery callback = new CallbackQuery()
+            {
+               Data = "/add 1",
+               Message = new Message()
+            };
+            
+            handler.TryHandleCallback(callback).Should().BeTrue();
+            
+            learningMethodology.ClearReceivedCalls();
+            wordsStorage.ClearReceivedCalls();
+            messanger.ClearReceivedCalls();
+            
+            handler.TryHandleCallback(callback).Should().BeFalse();
+            
+            learningMethodology.DidNotReceiveWithAnyArgs().CreateNewWord(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<WordForm>());
+            wordsStorage.DidNotReceiveWithAnyArgs().AddOrUpdate(Arg.Any<NewWordsBot.User>(), Arg.Any<Word>());
+            messanger.DidNotReceiveWithAnyArgs().SendNewWordConfirmation(Arg.Any<NewWordsBot.User>(), Arg.Any<Word>());
         }
         
         private static ILearningMethodology MockLearningMethodology()
         {
-            ILearningMethodology learningMethodology = Substitute.For<ILearningMethodology>();
-            return learningMethodology;
+            return Substitute.For<ILearningMethodology>();
         }
 
         private static INewWordsHandlerMessanger MockNewWordsHandlerMessanger()
         {
-            INewWordsHandlerMessanger messanger = Substitute.For<INewWordsHandlerMessanger>();
-            return messanger;
+            return Substitute.For<INewWordsHandlerMessanger>();
         }
 
         private static IUsersStorage MockUsersStorage()
         {
-            var usersStorage = Substitute.For<IUsersStorage>();
-            return usersStorage;
+            return Substitute.For<IUsersStorage>();
         }
 
         private static IWordsDictionary MockWordsDictionary()
         {
-            IWordsDictionary dictionary = Substitute.For<IWordsDictionary>();
-            return dictionary;
+            return Substitute.For<IWordsDictionary>();
         }
 
         private static IWordsStorage MockWordsStorage()
         {
-            var wordsStorage = Substitute.For<IWordsStorage>();
-            return wordsStorage;
+            return Substitute.For<IWordsStorage>();
         }
     }
 }
